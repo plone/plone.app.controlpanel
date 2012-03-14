9be14086b060e89387cf1c7b31977f6d6164afa5
Timo Stollenwerk <contact@timostollenwerk.net>
2012-03-14 22:29:57 +0100

    Use plone.app.testing for functional tests.

--- plone/app/controlpanel/tests/test_doctests.py
+++ plone/app/controlpanel/tests/test_doctests.py
@@ -1,43 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Functional Doctests for plone.app.controlpanel.
+"""
 import doctest
-from unittest import TestSuite
 
-from Products.PloneTestCase.PloneTestCase import setupPloneSite
-from Testing.ZopeTestCase import FunctionalDocFileSuite
+import unittest2 as unittest
+import pprint
 
-from plone.app.controlpanel.tests.cptc import ControlPanelTestCase
-from plone.app.controlpanel.tests.cptc import UserGroupsControlPanelTestCase
+from plone.testing import layered
 
-setupPloneSite()
+from plone.app.discussion.testing import \
+    PLONE_APP_DISCUSSION_FUNCTIONAL_TESTING
 
-OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+optionflags = (
+    doctest.ELLIPSIS | \
+    doctest.NORMALIZE_WHITESPACE | \
+    doctest.REPORT_ONLY_FIRST_FAILURE)
+normal_testfiles = [
+    'calendar.txt',
+    'editing.txt',
+    'filter.txt',
+    'mail.txt',
+    'maintenance.txt',
+    'security_enable_user_folder.txt',
+    'search.txt',
+    'site.txt',
+    'skins.txt',
+    'markup.txt',
+    'navigation.txt',
+    'types.txt',
+]
 
 
 def test_suite():
-    tests = ['calendar.txt',
-             'editing.txt',
-             'filter.txt',
-             'mail.txt',
-             'maintenance.txt',
-             'security_enable_user_folder.txt',
-             'search.txt',
-             'site.txt',
-             'skins.txt',
-             'markup.txt',
-             'navigation.txt',
-             'types.txt',
-             ]
-    suite = TestSuite()
-
-    for test in tests:
-        suite.addTest(FunctionalDocFileSuite(test,
-            optionflags=OPTIONFLAGS,
-            package="plone.app.controlpanel.tests",
-            test_class=ControlPanelTestCase))
-
-    suite.addTest(FunctionalDocFileSuite(
-        'usergroups.txt',
-        optionflags=OPTIONFLAGS,
-        package="plone.app.controlpanel.tests",
-        test_class=UserGroupsControlPanelTestCase))
-
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(doctest.DocFileSuite(test,
+                                     optionflags=optionflags,
+                                     globs={'pprint': pprint.pprint,
+                                            }
+                                     ),
+                layer=PLONE_APP_DISCUSSION_FUNCTIONAL_TESTING)
+        for test in normal_testfiles])
     return suite
+
