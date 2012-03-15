87beaf0c437d0f99738f5be5d4c621958b7f0be8
Timo Stollenwerk <contact@timostollenwerk.net>
2012-03-15 09:43:28 +0100

    Tests for EditingControlPanelAdapter added.

--- plone/app/controlpanel/tests/test_editing.py
+++ plone/app/controlpanel/tests/test_editing.py
@@ -95,42 +95,80 @@ class EditingControlPanelAdapterTest(unittest.TestCase):
         ptool = getToolByName(self.portal, 'portal_properties')
         self.site_properties = ptool.site_properties
 
-    def test_visible_ids_setting(self):
+    def test_get_visible_ids_setting(self):
         self.assertEquals(self.site_properties.visible_ids, False)
         self.site_properties.visible_ids = True
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.visible_ids, True)
 
-    def test_enable_inline_editing_setting(self):
+    def test_set_visible_ids_setting(self):
+        self.assertEquals(self.site_properties.visible_ids, False)
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        editing_settings.visible_ids = True
+        self.assertEquals(self.site_properties.visible_ids, True)
+
+    def test_get_enable_inline_editing_setting(self):
         self.assertEquals(self.site_properties.enable_inline_editing, False)
         self.site_properties.enable_inline_editing = True
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.enable_inline_editing, True)
 
-    def test_enable_link_integrity_checks_setting(self):
+    def test_set_enable_inline_editing_setting(self):
+        self.assertEquals(self.site_properties.enable_inline_editing, False)
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        editing_settings.enable_inline_editing = True
+        self.assertEquals(self.site_properties.enable_inline_editing, True)
+
+    def test_get_enable_link_integrity_checks_setting(self):
         self.assertEquals(
             self.site_properties.enable_link_integrity_checks, True)
         self.site_properties.enable_link_integrity_checks = False
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.enable_link_integrity_checks, False)
 
-    def test_ext_editor_setting(self):
+    def test_set_enable_link_integrity_checks_setting(self):
+        self.assertEquals(
+            self.site_properties.enable_link_integrity_checks, True)
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        editing_settings.enable_link_integrity_checks = False
+        self.assertEquals(
+            self.site_properties.enable_link_integrity_checks, False)
+
+    def test_get_ext_editor_setting(self):
         self.assertEquals(self.site_properties.ext_editor, False)
         self.site_properties.ext_editor = True
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.ext_editor, True)
 
-    def test_default_editor_setting(self):
-        self.assertEquals(self.site_properties.default_editor, "TinyMCE")
-        self.site_properties.default_editor = "Kupu"
+    def test_set_ext_editor_setting(self):
+        self.assertEquals(self.site_properties.ext_editor, False)
         editing_settings = getAdapter(self.portal, IEditingSchema)
-        self.assertEquals(editing_settings.default_editor, "Kupu")
+        editing_settings.ext_editor = True
+        self.assertEquals(self.site_properties.ext_editor, True)
 
-    def test_lock_on_ttw_edit_setting(self):
+    def test_get_default_editor_setting(self):
+        self.assertEquals(self.site_properties.default_editor, "TinyMCE")
+        self.site_properties.default_editor = "None"
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        self.assertEquals(editing_settings.default_editor, "None")
+
+    def test_set_default_editor_setting(self):
+        self.assertEquals(self.site_properties.default_editor, "TinyMCE")
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        editing_settings.default_editor = "None"
+        self.assertEquals(self.site_properties.default_editor, "None")
+
+    def test_get_lock_on_ttw_edit_setting(self):
         self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
         self.site_properties.lock_on_ttw_edit = False
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.lock_on_ttw_edit, False)
+
+    def test_set_lock_on_ttw_edit_setting(self):
+        self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
+        editing_settings = getAdapter(self.portal, IEditingSchema)
+        editing_settings.lock_on_ttw_edit = False
+        self.assertEquals(self.site_properties.lock_on_ttw_edit, False)
 
 
 def test_suite():
