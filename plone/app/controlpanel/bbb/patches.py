580c21d7f8e1bd0b8330a2b1bc20ca02f30d8a38
Timo Stollenwerk <contact@timostollenwerk.net>
2012-05-10 22:26:13 +0200

    Use updateProperty instead of setProperty. We want to update the registry settings when properties are set.

--- plone/app/controlpanel/bbb/patches.py
+++ plone/app/controlpanel/bbb/patches.py
@@ -1,16 +1,15 @@
-from zope.site.hooks import getSite
-from zope.component import getAdapter
+from zope.component import queryUtility
+
+from plone.registry.interfaces import IRegistry
+
 from Products.CMFPlone.PropertiesTool import SimpleItemWithProperties
-from plone.app.controlpanel.interfaces import IEditingSchema
+from plone.app.controlpanel.interfaces import IMailSchema
 
 
-def _setProperty(self, id, value, type='string'):
-    super(SimpleItemWithProperties, self)._setProperty(id, value, type=type)
-    portal = getSite()
-    if id in IEditingSchema.names():
-        settings = getAdapter(portal, IEditingSchema)
-        if value == '':
-            value = False
-        if id == 'default_editor' and value == False:
-            value = 'None'
-        setattr(settings, id, value)
+def _updateProperty(self, id, value):
+    super(SimpleItemWithProperties, self)._updateProperty(id, value)
+    registry = queryUtility(IRegistry, context=self)
+    if registry:
+        if id in IMailSchema.names():
+            settings = registry.forInterface(IMailSchema)
+            setattr(settings, id, value)
