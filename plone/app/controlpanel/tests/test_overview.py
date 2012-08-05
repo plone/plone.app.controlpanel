1be1b16e51ea0b1e53953ec0b50e967d6f830702
Timo Stollenwerk <contact@timostollenwerk.net>
2012-08-05 15:00:07 +0200

    Move overview control panel to browser directory.

---
+++ plone/app/controlpanel/tests/test_overview.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import unittest2 as unittest
+
+from plone.testing.z2 import Browser
+from plone.app.testing import SITE_OWNER_NAME, SITE_OWNER_PASSWORD
+
+from plone.app.controlpanel.testing import \
+    PLONE_APP_CONTROLPANEL_FUNCTIONAL_TESTING
+
+
+class OverviewControlPanelFunctionalTest(unittest.TestCase):
+
+    layer = PLONE_APP_CONTROLPANEL_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.portal_url = self.portal.absolute_url()
+        self.browser = Browser(self.app)
+        self.browser.handleErrors = False
+        self.browser.addHeader('Authorization',
+                'Basic %s:%s' % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD,)
+            )
+
+    def test_overview_control_panel_link(self):
+        self.browser.open(self.portal_url)
+        self.browser.getLink('Site Setup').click()
+        self.assertTrue("Site Setup" in self.browser.contents)
+        self.assertTrue(
+            "Configuration area for Plone and add-on Products." \
+            in self.browser.contents)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
