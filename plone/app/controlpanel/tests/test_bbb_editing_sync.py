323e7b9790e0d78377173577745659a2d397bab6
Timo Stollenwerk <contact@timostollenwerk.net>
2012-08-08 11:54:05 +0200

    properties to registry sync bbb tests added.

--- plone/app/controlpanel/tests/test_bbb_editing_sync.py
+++ plone/app/controlpanel/tests/test_bbb_editing_sync.py
@@ -25,19 +25,19 @@ class SyncPloneAppRegistryToEditingSitePropertiesTest(unittest.TestCase):
         registry = queryUtility(IRegistry)
         self.settings = registry.forInterface(IEditingSchema)
 
-    def test_set_visible_ids_property(self):
+    def test_sync_visible_ids_property(self):
         self.assertEquals(self.site_properties.visible_ids, False)
         self.assertEquals(self.settings.visible_ids, False)
         self.settings.visible_ids = True
         self.assertEquals(self.site_properties.visible_ids, True)
 
-    def test_set_enable_inline_editing_property(self):
+    def test_sync_enable_inline_editing_property(self):
         self.assertEquals(self.site_properties.enable_inline_editing, False)
         self.assertEquals(self.settings.enable_inline_editing, False)
         self.settings.enable_inline_editing = True
         self.assertEquals(self.site_properties.enable_inline_editing, True)
 
-    def test_set_enable_link_integrity_checks_property(self):
+    def test_sync_enable_link_integrity_checks_property(self):
         self.assertEquals(
             self.site_properties.enable_link_integrity_checks, True)
         self.assertEquals(
@@ -46,20 +46,73 @@ class SyncPloneAppRegistryToEditingSitePropertiesTest(unittest.TestCase):
         self.assertEquals(
             self.site_properties.enable_link_integrity_checks, False)
 
-    def test_set_ext_editor_property(self):
+    def test_sync_ext_editor_property(self):
         self.assertEquals(self.site_properties.ext_editor, False)
         self.assertEquals(self.settings.ext_editor, False)
         self.settings.ext_editor = True
         self.assertEquals(self.site_properties.ext_editor, True)
 
-    def test_set_default_editor_property(self):
+    def test_sync_default_editor_property(self):
         self.assertEquals(self.site_properties.default_editor, 'TinyMCE')
         self.assertEquals(self.settings.default_editor, 'TinyMCE')
         self.settings.default_editor = 'None'
         self.assertEquals(self.site_properties.default_editor, 'None')
 
-    def test_set_lock_on_ttw_edit_property(self):
+    def test_sync_lock_on_ttw_edit_property(self):
         self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
         self.assertEquals(self.settings.lock_on_ttw_edit, True)
         self.settings.lock_on_ttw_edit = False
         self.assertEquals(self.site_properties.lock_on_ttw_edit, False)
+
+
+class SyncEditingSitePropertiesToPloneAppRegistryTest(unittest.TestCase):
+
+    layer = PLONE_APP_CONTROLPANEL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        ptool = getToolByName(self.portal, 'portal_properties')
+        self.site_properties = ptool.site_properties
+        registry = queryUtility(IRegistry)
+        self.settings = registry.forInterface(IEditingSchema)
+
+    def test_sync_visible_ids_property(self):
+        self.assertEquals(self.site_properties.visible_ids, False)
+        self.assertEquals(self.settings.visible_ids, False)
+        self.site_properties.visible_ids = True
+        self.assertEquals(self.settings.visible_ids, True)
+
+    def test_sync_enable_inline_editing_property(self):
+        self.assertEquals(self.site_properties.enable_inline_editing, False)
+        self.assertEquals(self.settings.enable_inline_editing, False)
+        self.site_properties.enable_inline_editing = True
+        self.assertEquals(self.settings.enable_inline_editing, True)
+
+    def test_sync_enable_link_integrity_checks_property(self):
+        self.assertEquals(
+            self.site_properties.enable_link_integrity_checks, True)
+        self.assertEquals(
+            self.settings.enable_link_integrity_checks, True)
+        self.site_properties.enable_link_integrity_checks = False
+        self.assertEquals(
+            self.settings.enable_link_integrity_checks, False)
+
+    def test_sync_ext_editor_property(self):
+        self.assertEquals(self.site_properties.ext_editor, False)
+        self.assertEquals(self.settings.ext_editor, False)
+        self.site_properties.ext_editor = True
+        self.assertEquals(self.settings.ext_editor, True)
+
+    def test_sync_default_editor_property(self):
+        self.assertEquals(self.site_properties.default_editor, 'TinyMCE')
+        self.assertEquals(self.settings.default_editor, 'TinyMCE')
+        self.site_properties.default_editor = 'None'
+        self.assertEquals(self.settings.default_editor, 'None')
+
+    def test_sync_lock_on_ttw_edit_property(self):
+        self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
+        self.assertEquals(self.settings.lock_on_ttw_edit, True)
+        self.site_properties.lock_on_ttw_edit = False
+        self.assertEquals(self.settings.lock_on_ttw_edit, False)
