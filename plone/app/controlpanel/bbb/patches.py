91e3f2ec7fa70b4922e08c9599ec006718fd9255
Timo Stollenwerk <contact@timostollenwerk.net>
2012-08-10 10:52:20 +0200

    Patch _setPropValue instead of _updateProperty to make sure we catch all changes.

--- plone/app/controlpanel/bbb/patches.py
+++ plone/app/controlpanel/bbb/patches.py
@@ -1,18 +1,21 @@
-
 from zope.component import queryUtility
 
 from plone.registry.interfaces import IRegistry
 
 from Products.CMFPlone.PropertiesTool import SimpleItemWithProperties
 
+from plone.app.controlpanel.interfaces import IEditingSchema
 from plone.app.controlpanel.interfaces import IMailSchema
 from plone.app.controlpanel.interfaces import INavigationSchema
 
 
-def _updateProperty(self, id, value):
-    super(SimpleItemWithProperties, self)._updateProperty(id, value)
+def _setPropValue(self, id, value):
+    super(SimpleItemWithProperties, self)._setPropValue(id, value)
     registry = queryUtility(IRegistry, context=self)
     if registry:
+        if id in IEditingSchema.names():
+            settings = registry.forInterface(IEditingSchema)
+            setattr(settings, id, value)
         if id in IMailSchema.names():
             settings = registry.forInterface(IMailSchema)
             setattr(settings, id, value)
