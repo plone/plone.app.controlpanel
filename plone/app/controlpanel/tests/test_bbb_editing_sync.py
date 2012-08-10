fa0521a32f376842c02617c9d794266817b5e15e
Timo Stollenwerk <contact@timostollenwerk.net>
2012-08-10 10:50:34 +0200

    The PropertyManager does not support the direct assignment of attributes. We use manage_changeProperties instead.

--- plone/app/controlpanel/tests/test_bbb_editing_sync.py
+++ plone/app/controlpanel/tests/test_bbb_editing_sync.py
@@ -81,13 +81,14 @@ class SyncEditingSitePropertiesToPloneAppRegistryTest(unittest.TestCase):
     def test_sync_visible_ids_property(self):
         self.assertEquals(self.site_properties.visible_ids, False)
         self.assertEquals(self.settings.visible_ids, False)
-        self.site_properties.visible_ids = True
+        self.site_properties.manage_changeProperties(visible_ids=True)
         self.assertEquals(self.settings.visible_ids, True)
 
     def test_sync_enable_inline_editing_property(self):
         self.assertEquals(self.site_properties.enable_inline_editing, False)
         self.assertEquals(self.settings.enable_inline_editing, False)
-        self.site_properties.enable_inline_editing = True
+        self.site_properties.manage_changeProperties(
+            enable_inline_editing=True)
         self.assertEquals(self.settings.enable_inline_editing, True)
 
     def test_sync_enable_link_integrity_checks_property(self):
@@ -95,24 +96,28 @@ class SyncEditingSitePropertiesToPloneAppRegistryTest(unittest.TestCase):
             self.site_properties.enable_link_integrity_checks, True)
         self.assertEquals(
             self.settings.enable_link_integrity_checks, True)
-        self.site_properties.enable_link_integrity_checks = False
+        self.site_properties.manage_changeProperties(
+            enable_link_integrity_checks=False)
         self.assertEquals(
             self.settings.enable_link_integrity_checks, False)
 
     def test_sync_ext_editor_property(self):
         self.assertEquals(self.site_properties.ext_editor, False)
         self.assertEquals(self.settings.ext_editor, False)
-        self.site_properties.ext_editor = True
+        self.site_properties.manage_changeProperties(
+            ext_editor=True)
         self.assertEquals(self.settings.ext_editor, True)
 
     def test_sync_default_editor_property(self):
         self.assertEquals(self.site_properties.default_editor, 'TinyMCE')
         self.assertEquals(self.settings.default_editor, 'TinyMCE')
-        self.site_properties.default_editor = 'None'
+        self.site_properties.manage_changeProperties(
+            default_editor='None')
         self.assertEquals(self.settings.default_editor, 'None')
 
     def test_sync_lock_on_ttw_edit_property(self):
         self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
         self.assertEquals(self.settings.lock_on_ttw_edit, True)
-        self.site_properties.lock_on_ttw_edit = False
+        self.site_properties.manage_changeProperties(
+            lock_on_ttw_edit=False)
         self.assertEquals(self.settings.lock_on_ttw_edit, False)
