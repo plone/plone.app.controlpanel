7f7d27b53224c86bb757036f6514ea76ed7a471b
Timo Stollenwerk <contact@timostollenwerk.net>
2012-08-12 10:08:30 +0200

    Sync MailHost changes with registry.

--- plone/app/controlpanel/bbb/patches.py
+++ plone/app/controlpanel/bbb/patches.py
@@ -1,5 +1,4 @@
 from zope.component import queryUtility
-
 from plone.registry.interfaces import IRegistry
 
 from Products.CMFPlone.PropertiesTool import SimpleItemWithProperties
@@ -22,3 +21,29 @@ def _setPropValue(self, id, value):
         if id in INavigationSchema.names():
             settings = registry.forInterface(INavigationSchema)
             setattr(settings, id, value)
+
+
+def manage_makeChanges(self, title, smtp_host, smtp_port, smtp_uid='',
+    smtp_pwd='', smtp_queue=False, smtp_queue_directory='/tmp',
+    force_tls=False, REQUEST=None):
+    #super(MailBase, self).manage_makeChanges(
+    #    title,
+    #    smtp_host,
+    #    smtp_port,
+    #    smtp_uid,
+    #    smtp_pwd,
+    #    smtp_queue,
+    #    smtp_queue_directory,
+    #    force_tls,
+    #    REQUEST)
+    registry = queryUtility(IRegistry, context=self)
+    settings = registry.forInterface(IMailSchema)
+    if registry:
+        if smtp_host:
+            setattr(settings, "smtp_host", smtp_host.decode("utf-8"))
+        if smtp_port:
+            setattr(settings, "smtp_port", smtp_port)
+        if smtp_uid:
+            setattr(settings, "smtp_userid", smtp_uid)
+        if smtp_pwd:
+            setattr(settings, "smtp_pass", smtp_pwd)
--- plone/app/controlpanel/tests/test_bbb_mail_sync.py
+++ plone/app/controlpanel/tests/test_bbb_mail_sync.py
@@ -29,41 +29,117 @@ class TestSyncPloneAppRegistryToMailhostProperties(unittest.TestCase):
         self.mailhost = getToolByName(self.portal, "MailHost")
 
     def test_sync_smtp_host(self):
-        self.assertEquals(self.mailhost.smtp_host, "")
-        self.assertEquals(self.settings.smtp_host, u"localhost")
+        self.assertEqual(self.mailhost.smtp_host, "")
+        self.assertEqual(self.settings.smtp_host, u"localhost")
         self.settings.smtp_host = u"http://myhost.com"
-        self.assertEquals(self.mailhost.smtp_host, "http://myhost.com")
+        self.assertEqual(self.mailhost.smtp_host, "http://myhost.com")
 
     def test_sync_smtp_port(self):
-        self.assertEquals(self.mailhost.smtp_port, 25)
-        self.assertEquals(self.settings.smtp_port, 25)
+        self.assertEqual(self.mailhost.smtp_port, 25)
+        self.assertEqual(self.settings.smtp_port, 25)
         self.settings.smtp_port = 2525
-        self.assertEquals(self.mailhost.smtp_port, 2525)
+        self.assertEqual(self.mailhost.smtp_port, 2525)
 
     def test_sync_smtp_userid(self):
-        self.assertEquals(self.mailhost.smtp_uid, "")
-        self.assertEquals(self.settings.smtp_userid, None)
+        self.assertEqual(self.mailhost.smtp_uid, "")
+        self.assertEqual(self.settings.smtp_userid, None)
         self.settings.smtp_userid = u"johndoe"
-        self.assertEquals(self.mailhost.smtp_uid, u"johndoe")
+        self.assertEqual(self.mailhost.smtp_uid, u"johndoe")
 
     def test_sync_smtp_pass(self):
-        self.assertEquals(self.mailhost.smtp_pwd, "")
-        self.assertEquals(self.settings.smtp_pass, None)
+        self.assertEqual(self.mailhost.smtp_pwd, "")
+        self.assertEqual(self.settings.smtp_pass, None)
         self.settings.smtp_pass = u"secret"
-        self.assertEquals(self.mailhost.smtp_pwd, u"secret")
+        self.assertEqual(self.mailhost.smtp_pwd, u"secret")
 
     def test_sync_email_form_name(self):
-        self.assertEquals(getUtility(ISiteRoot).email_from_name, "")
-        self.assertEquals(self.settings.email_from_name, None)
+        self.assertEqual(getUtility(ISiteRoot).email_from_name, "")
+        self.assertEqual(self.settings.email_from_name, None)
         self.settings.email_from_name = u"Site Administrator"
-        self.assertEquals(
+        self.assertEqual(
             getUtility(ISiteRoot).email_from_name,
             u"Site Administrator")
 
     def test_sync_email_form_address(self):
-        self.assertEquals(getUtility(ISiteRoot).email_from_address, "")
-        self.assertEquals(self.settings.email_from_address, None)
+        self.assertEqual(getUtility(ISiteRoot).email_from_address, "")
+        self.assertEqual(self.settings.email_from_address, None)
         self.settings.email_from_address = "admin@plone.org"
-        self.assertEquals(
+        self.assertEqual(
             getUtility(ISiteRoot).email_from_address,
             "admin@plone.org")
+
+
+class TestSyncMailhostPropertiesToPloneAppRegistry(unittest.TestCase):
+
+    layer = PLONE_APP_CONTROLPANEL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        ptool = getToolByName(self.portal, 'portal_properties')
+        self.site_properties = ptool.site_properties
+        registry = queryUtility(IRegistry)
+        self.settings = registry.forInterface(IMailSchema)
+        self.mailhost = getToolByName(self.portal, "MailHost")
+
+    def test_sync_smtp_host(self):
+        self.assertEqual(self.mailhost.smtp_host, "")
+        self.assertEqual(self.settings.smtp_host, u"localhost")
+        self.mailhost.manage_makeChanges(
+            "Plone Mail Host",
+            smtp_port=25,
+            smtp_host="http://myhost.com")
+        self.assertEqual(self.mailhost.smtp_host, "http://myhost.com")
+        self.assertEqual(self.settings.smtp_host, "http://myhost.com")
+
+    def test_sync_smtp_port(self):
+        self.assertEqual(self.mailhost.smtp_port, 25)
+        self.assertEqual(self.settings.smtp_port, 25)
+        self.mailhost.manage_makeChanges(
+            "Plone Mail Host",
+            smtp_port=2525,
+            smtp_host="http://myhost.com")
+        self.assertEqual(self.mailhost.smtp_port, 2525)
+        self.assertEqual(self.settings.smtp_port, 2525)
+
+    def test_sync_smtp_userid(self):
+        self.assertEqual(self.mailhost.smtp_uid, "")
+        self.assertEqual(self.settings.smtp_userid, None)
+        self.mailhost.manage_makeChanges(
+            "Plone Mail Host",
+            smtp_port=2525,
+            smtp_host="http://myhost.com",
+            smtp_uid=u"johndoe")
+        self.assertEqual(self.mailhost.smtp_uid, u"johndoe")
+        self.assertEqual(self.settings.smtp_userid, u"johndoe")
+
+    def test_sync_smtp_pass(self):
+        self.assertEqual(self.mailhost.smtp_pwd, "")
+        self.assertEqual(self.settings.smtp_pass, None)
+        self.mailhost.manage_makeChanges(
+            "Plone Mail Host",
+            smtp_port=2525,
+            smtp_host="http://myhost.com",
+            smtp_uid=u"johndoe",
+            smtp_pwd=u"secret")
+        self.assertEqual(self.mailhost.smtp_pwd, u"secret")
+        self.assertEqual(self.settings.smtp_pass, u"secret")
+
+    def test_sync_email_form_name(self):
+        self.assertEqual(getUtility(ISiteRoot).email_from_name, "")
+        self.assertEqual(self.settings.email_from_name, None)
+        getUtility(ISiteRoot).email_from_name = u"Site Administrator"
+        self.assertEqual(
+            getUtility(ISiteRoot).email_from_name,
+            u"Site Administrator")
+        self.assertEqual(self.settings.email_from_name, u"Site Administrator")
+
+    def test_sync_email_form_address(self):
+        self.assertEqual(getUtility(ISiteRoot).email_from_address, "")
+        self.assertEqual(self.settings.email_from_address, None)
+        getUtility(ISiteRoot).email_from_address = "admin@plone.org"
+        self.assertEqual(
+            getUtility(ISiteRoot).email_from_address,
+            "admin@plone.org")
+        self.assertEqual(self.settings.email_from_address, u"admin@plone.org")
