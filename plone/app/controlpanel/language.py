122166edeb7c755b8570321fd3ae214e9f3e2c2c
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-01-25 11:25:08 +0100

    expose the rest of portal_languages options exposed n the ZMI in a control panel

--- plone/app/controlpanel/language.py
+++ plone/app/controlpanel/language.py
@@ -1,11 +1,14 @@
+from zope.event import notify
 from plone.app.form.widgets import LanguageDropdownChoiceWidget
 
 from zope.formlib.form import FormFields
+from zope.formlib import form
 from zope.interface import Interface
 from zope.component import adapts
 from zope.interface import implements
 from zope.schema import Bool
 from zope.schema import Choice
+from zope.schema import List
 
 from Acquisition import aq_inner
 
@@ -15,7 +18,10 @@ from Products.CMFPlone import PloneMessageFactory as _
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.statusmessages.interfaces import IStatusMessage
 
+from plone.app.controlpanel.events import ConfigurationChangedEvent
 from plone.app.controlpanel.form import ControlPanelForm
+
+from plone.protect import CheckAuthenticator
 
 
 class ILanguageSelectionSchema(Interface):
@@ -37,6 +43,93 @@ class ILanguageSelectionSchema(Interface):
                                "of this site."),
         required=True,
         vocabulary="plone.app.vocabularies.AvailableContentLanguages")
+
+    available_languages = List(
+        title=_(u"heading_available_languages",
+                default=u"Available languages"),
+        description=_(u"description_available_languages",
+                default=u"The languages in which the site should be "
+                        u"translatable."),
+        required=True,
+        missing_value=set(),
+        value_type=Choice(
+            vocabulary=("plone.app.vocabularies.AvailableContentLanguages")))
+
+    use_content_negotiation = Bool(
+        title=_(u"heading_language_of_the_content",
+                default=u"Use the language of the content item."),
+        description=_(u"description_language_of_the_content",
+                default=u"Use the language of the content item."),
+        required=False,
+        )
+
+    use_path_negotiation = Bool(
+        title=_(
+            u"heading_language_codes_in_URL",
+            default=u"Use language codes in URL path for manual override."),
+        description=_(
+            u"description_language_codes_in_URL",
+            default=u"Use language codes in URL path for manual override."),
+        required=False,
+        )
+
+    use_cookie_negotiation = Bool(
+        title=_(u"heading_cookie_manual_override",
+                default=(u"Use cookie for manual override. (Required for "
+                         u"the language selector viewlet to be rendered.)")),
+        description=_(
+            u"description_cookie_manual_override",
+            default=(u"Use cookie for manual override. (Required for the "
+                     u"language selector viewlet to be rendered.)")),
+        required=False,
+        )
+
+    authenticated_users_only = Bool(
+        title=_(u"heading_auth_cookie_manual_override",
+                default=u"Authenticated users only."),
+        description=_(
+            u"description_auth_ookie_manual_override",
+            default=(u"Authenticated users only. Use cookie for manual "
+                     u"override. (Required for the language selector viewlet "
+                     u"to be rendered.)")),
+        required=False,
+        )
+
+    set_cookie_everywhere = Bool(
+        title=_(
+            u"heading_set_language_cookie_always",
+            default=(u"Set the language cookie always, i.e. also when the "
+                     u"'set_language' request parameter is absent.")),
+        description=_(
+            u"description_set_language_cookie_always",
+            default=(u"Set the language cookie always, i.e. also when the "
+                     u"'set_language' request parameter is absent.")),
+        required=False,
+        )
+
+    use_subdomain_negotiation = Bool(
+        title=_(u"heading_use_subdomain",
+                default=u"Use subdomain (e.g.: de.plone.org)."),
+        description=_(u"description_use_subdomain",
+                default=u"Use subdomain (e.g.: de.plone.org)."),
+        required=False,
+        )
+
+    use_cctld_negotiation = Bool(
+        title=_(u"heading_top_level_domain",
+                default=u"Use top-level domain (e.g.: www.plone.de)."),
+        description=_(u"description_top_level_domain",
+                default=u"Use top-level domain (e.g.: www.plone.de)."),
+        required=False,
+        )
+
+    use_request_negotiation = Bool(
+        title=_(u"heading_browser_language_request_negotiation",
+                default=u"Use browser language request negotiation."),
+        description=_(u"description_browser_language_request_negotiation",
+                default=u"Use browser language request negotiation."),
+        required=False,
+        )
 
 
 class LanguageControlPanelAdapter(SchemaAdapterBase):
@@ -60,9 +153,6 @@ class LanguageControlPanelAdapter(SchemaAdapterBase):
             context.supported_langs = [value]
         context.setDefaultLanguage(value)
 
-    default_language = property(get_default_language,
-                                set_default_language)
-
     def get_use_combined_language_codes(self):
         return aq_inner(self.context).use_combined_language_codes
 
@@ -82,8 +172,86 @@ class LanguageControlPanelAdapter(SchemaAdapterBase):
         else:
             context.use_combined_language_codes = value
 
+    def get_available_languages(self):
+        return [unicode(l) for l in self.context.getSupportedLanguages()]
+
+    def set_available_languages(self, value):
+        languages = [str(l) for l in value]
+        self.context.supported_langs = languages
+
+    def get_use_content_negotiation(self):
+        return self.context.use_content_negotiation
+
+    def set_use_content_negotiation(self, value):
+        self.context.use_content_negotiation = value
+
+    def get_use_path_negotiation(self):
+        return self.context.use_path_negotiation
+
+    def set_use_path_negotiation(self, value):
+        self.context.use_path_negotiation = value
+
+    def get_use_cookie_negotiation(self):
+        return self.context.use_cookie_negotiation
+
+    def set_use_cookie_negotiation(self, value):
+        self.context.use_cookie_negotiation = value
+
+    def get_authenticated_users_only(self):
+        return self.context.authenticated_users_only
+
+    def set_authenticated_users_only(self, value):
+        self.context.authenticated_users_only = value
+
+    def get_set_cookie_everywhere(self):
+        return self.context.set_cookie_everywhere
+
+    def set_set_cookie_everywhere(self, value):
+        self.context.set_cookie_everywhere = value
+
+    def get_use_subdomain_negotiation(self):
+        return self.context.use_subdomain_negotiation
+
+    def set_use_subdomain_negotiation(self, value):
+        self.context.use_subdomain_negotiation = value
+
+    def get_use_cctld_negotiation(self):
+        return self.context.use_cctld_negotiation
+
+    def set_use_cctld_negotiation(self, value):
+        self.context.use_cctld_negotiation = value
+
+    def get_use_request_negotiation(self):
+        return self.context.use_request_negotiation
+
+    def set_use_request_negotiation(self, value):
+        self.context.use_request_negotiation = value
+
+    default_language = property(get_default_language,
+                                set_default_language)
+
     use_combined_language_codes = property(get_use_combined_language_codes,
                                            set_use_combined_language_codes)
+
+    available_languages = property(get_available_languages,
+                                   set_available_languages)
+
+    use_content_negotiation = property(get_use_content_negotiation,
+                                       set_use_content_negotiation)
+    use_path_negotiation = property(get_use_path_negotiation,
+                                    set_use_path_negotiation)
+    use_cookie_negotiation = property(get_use_cookie_negotiation,
+                                      set_use_cookie_negotiation)
+    authenticated_users_only = property(get_authenticated_users_only,
+                                        set_authenticated_users_only)
+    set_cookie_everywhere = property(get_set_cookie_everywhere,
+                                     set_set_cookie_everywhere)
+    use_subdomain_negotiation = property(get_use_subdomain_negotiation,
+                                         set_use_subdomain_negotiation)
+    use_cctld_negotiation = property(get_use_cctld_negotiation,
+                                     set_use_cctld_negotiation)
+    use_request_negotiation = property(get_use_request_negotiation,
+                                       set_use_request_negotiation)
 
 
 class LanguageControlPanel(ControlPanelForm):
@@ -96,3 +264,4 @@ class LanguageControlPanel(ControlPanelForm):
                     default="Settings related to interface languages and "
                             "content translations.")
     form_name = _(u"heading_language_settings", default="Language Settings")
+
