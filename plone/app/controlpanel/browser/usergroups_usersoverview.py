79d2feb638a54ae1e3a90231e85840e96b773980
Timo Stollenwerk <contact@timostollenwerk.net>
2013-01-27 15:16:32 +0100

    Factor out UsersGroupsControlPanelView.

--- plone/app/controlpanel/browser/usergroups_usersoverview.py
+++ plone/app/controlpanel/browser/usergroups_usersoverview.py
@@ -1,8 +1,3 @@
-from Products.CMFCore.permissions import ManagePortal
-from ZTUtils import make_query
-from zope.component import getAdapter
-from AccessControl import getSecurityManager
-from plone.app.controlpanel.interfaces import ISecuritySchema
 import logging
 from Acquisition import aq_inner
 from zExceptions import Forbidden
@@ -13,105 +8,15 @@ from zope.component import getUtility
 from plone.protect import CheckAuthenticator
 from zope.component import getMultiAdapter
 from Products.CMFCore.interfaces import ISiteRoot
-from Products.Five.browser import BrowserView
 from plone.app.controlpanel import _
 from Products.CMFCore.utils import getToolByName
 
 from Products.CMFPlone.utils import normalizeString
-
+from plone.app.controlpanel.browser.usergroups import (
+    UsersGroupsControlPanelView
+)
 
 logger = logging.getLogger('plone.app.controlpanel')
-
-
-class UsersGroupsControlPanelView(BrowserView):
-
-    @property
-    def portal_roles(self):
-        pmemb = getToolByName(aq_inner(self.context), 'portal_membership')
-        return [r for r in pmemb.getPortalRoles() if r != 'Owner']
-
-    @property
-    def many_users(self):
-        pprop = getToolByName(aq_inner(self.context), 'portal_properties')
-        return pprop.site_properties.many_users
-
-    @property
-    def many_groups(self):
-        pprop = getToolByName(aq_inner(self.context), 'portal_properties')
-        return pprop.site_properties.many_groups
-
-    @property
-    def email_as_username(self):
-        return getAdapter(aq_inner(self.context), ISecuritySchema).get_use_email_as_login()
-
-    def makeQuery(self, **kw):
-        return make_query(**kw)
-
-    def membershipSearch(self, searchString='', searchUsers=True, searchGroups=True, ignore=[]):
-        """Search for users and/or groups, returning actual member and group items
-           Replaces the now-deprecated prefs_user_groups_search.py script"""
-        groupResults = userResults = []
-
-        gtool = getToolByName(self, 'portal_groups')
-        mtool = getToolByName(self, 'portal_membership')
-
-        searchView = getMultiAdapter((aq_inner(self.context), self.request), name='pas_search')
-
-        if searchGroups:
-            groupResults = searchView.merge(chain(*[searchView.searchGroups(**{field: searchString}) for field in ['id', 'title']]), 'groupid')
-            groupResults = [gtool.getGroupById(g['id']) for g in groupResults if g['id'] not in ignore]
-            groupResults.sort(key=lambda x: x is not None and normalizeString(x.getGroupTitleOrName()))
-
-        if searchUsers:
-            userResults = searchView.merge(chain(*[searchView.searchUsers(**{field: searchString}) for field in ['login', 'fullname', 'email']]), 'userid')
-            userResults = [mtool.getMemberById(u['id']) for u in userResults if u['id'] not in ignore]
-            userResults.sort(key=lambda x: x is not None and x.getProperty('fullname') is not None and normalizeString(x.getProperty('fullname')) or '')
-
-        return groupResults + userResults
-
-    def atoi(self, s):
-        try:
-            return int(s)
-        except ValueError:
-            return 0
-
-    @property
-    def is_zope_manager(self):
-        return getSecurityManager().checkPermission(ManagePortal, self.context)
-
-    # The next two class methods implement the following truth table:
-    #
-    # MANY USERS/GROUPS SEARCHING       CAN LIST USERS/GROUPS   RESULT
-    # False             False           False                   Lists unavailable
-    # False             False           True                    Show all
-    # False             True            False                   Show matching
-    # False             True            True                    Show matching
-    # True              False           False                   Too many to list
-    # True              False           True                    Lists unavailable
-    # True              True            False                   Show matching
-    # True              True            True                    Show matching
-
-    # TODO: Maybe have these methods return a text message (instead of a bool)
-    # corresponding to the actual result, e.g. "Too many to list", "Lists unavailable"
-
-    @property
-    def show_group_listing_warning(self):
-        if not self.searchString:
-            acl = getToolByName(self, 'acl_users')
-            if acl.canListAllGroups():
-                if self.many_groups:
-                    return True
-        return False
-
-    @property
-    def show_users_listing_warning(self):
-        if not self.searchString:
-            acl = getToolByName(self, 'acl_users')
-            # XXX Huh? Is canListAllUsers broken?
-            if not acl.canListAllUsers():
-                if self.many_users:
-                    return True
-        return False
 
 
 class UsersOverviewControlPanel(UsersGroupsControlPanelView):
@@ -147,14 +52,18 @@ class UsersOverviewControlPanel(UsersGroupsControlPanelView):
 
         mtool = getToolByName(self, 'portal_membership')
 
-        searchView = getMultiAdapter((aq_inner(self.context), self.request), name='pas_search')
+        searchView = getMultiAdapter((
+            aq_inner(self.context),
+            self.request
+        ), name='pas_search')
 
         # First, search for all inherited roles assigned to each group.
         # We push this in the request so that IRoles plugins are told provide
         # the roles inherited from the groups to which the principal belongs.
         self.request.set('__ignore_group_roles__', False)
         self.request.set('__ignore_direct_roles__', True)
-        inheritance_enabled_users = searchView.merge(chain(*[searchView.searchUsers(**{field: searchString}) for field in ['login', 'fullname', 'email']]), 'userid')
+        inheritance_enabled_users = searchView.merge(
+            chain(*[searchView.searchUsers(**{field: searchString}) for field in ['login', 'fullname', 'email']]), 'userid')
         allInheritedRoles = {}
         for user_info in inheritance_enabled_users:
             userId = user_info['id']
@@ -172,10 +81,13 @@ class UsersOverviewControlPanel(UsersGroupsControlPanelView):
         # the roles from the groups the principal belongs.
         self.request.set('__ignore_group_roles__', True)
         self.request.set('__ignore_direct_roles__', False)
-        explicit_users = searchView.merge(chain(*[searchView.searchUsers(**{field: searchString}) for field in ['login', 'fullname', 'email']]), 'userid')
+        explicit_users = searchView.merge(
+            chain(*[searchView.searchUsers(**{field: searchString}) for field in ['login', 'fullname', 'email']]), 'userid'
+        )
 
         # Tack on some extra data, including whether each role is explicitly
-        # assigned ('explicit'), inherited ('inherited'), or not assigned at all (None).
+        # assigned ('explicit'), inherited ('inherited'), or not assigned at
+        # all (None).
         results = []
         for user_info in explicit_users:
             userId = user_info['id']
@@ -315,5 +227,9 @@ class UsersOverviewControlPanel(UsersGroupsControlPanelView):
                 mdtool.deleteMemberData(member_id)
 
         # Delete members' local roles.
-        mtool.deleteLocalRoles( getUtility(ISiteRoot), member_ids,
-                               reindex=1, recursive=1 )
+        mtool.deleteLocalRoles(
+            getUtility(ISiteRoot),
+            member_ids,
+            reindex=1,
+            recursive=1 
+        )
