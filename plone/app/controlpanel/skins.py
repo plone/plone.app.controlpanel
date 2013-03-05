3bc96b0336fb3b4d1613dffd72d540ce6497cf6f
Timo Stollenwerk <contact@timostollenwerk.net>
2013-03-05 08:32:20 +0100

    Re-add import that newer versions of p.a.theming use.

---
+++ plone/app/controlpanel/skins.py
@@ -0,0 +1,3 @@
+# XXX: The newer versions of p.a.theming use this import. Needs to be marked
+# as deprecated or removed from p.a.theming.
+from plone.app.controlpanel.interfaces import ISkinsSchema
