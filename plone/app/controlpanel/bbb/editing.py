5f7db2581deca54f29916e13e03ee862908c8e3a
tisto <tisto@plone.org>
2013-11-17 13:25:25 +0100

    Make editing adapter set/get registry settings.

--- plone/app/controlpanel/bbb/editing.py
+++ plone/app/controlpanel/bbb/editing.py
@@ -2,6 +2,8 @@ from zope.component import adapts
 from Products.CMFDefault.formlib.schema import ProxyFieldProperty
 from zope.interface import implements
 from zope.site.hooks import getSite
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
@@ -14,18 +16,43 @@ class EditingControlPanelAdapter(object):
     implements(IEditingSchema)
 
     def __init__(self, context):
-        self.context = context
-        self.portal = getSite()
-        pprop = getToolByName(self.portal, 'portal_properties')
-        self.context = pprop.site_properties
-        self.encoding = pprop.site_properties.default_charset
+        registry = getUtility(IRegistry)
+        self.settings = registry.forInterface(IEditingSchema)
 
-    visible_ids = ProxyFieldProperty(IEditingSchema['visible_ids'])
-    enable_link_integrity_checks = ProxyFieldProperty(
-        IEditingSchema['enable_link_integrity_checks'])
-    ext_editor = ProxyFieldProperty(
-        IEditingSchema['ext_editor'])
-    default_editor = ProxyFieldProperty(
-        IEditingSchema['default_editor'])
-    lock_on_ttw_edit = ProxyFieldProperty(
-        IEditingSchema['lock_on_ttw_edit'])
+    def get_visible_ids(self):
+        return self.settings.visible_ids
+
+    def set_visible_ids(self, value):
+        self.settings.visible_ids = value
+
+    def get_enable_link_integrity_checks(self):
+        return self.settings.enable_link_integrity_checks
+
+    def set_enable_link_integrity_checks(self, value):
+        self.settings.enable_link_integrity_checks = value
+
+    def get_ext_editor(self):
+        return self.settings.ext_editor
+
+    def set_ext_editor(self, value):
+        self.settings.ext_editor = value
+
+    def get_default_editor(self):
+        return self.settings.default_editor
+
+    def set_default_editor(self, value):
+        self.settings.default_editor = value
+
+    def get_lock_on_ttw_edit(self):
+        return self.settings.lock_on_ttw_edit
+
+    def set_lock_on_ttw_edit(self, value):
+        self.settings.lock_on_ttw_edit = value
+
+    visible_ids = property(get_visible_ids, set_visible_ids)
+    enable_link_integrity_checks = property(
+        get_enable_link_integrity_checks,
+        set_enable_link_integrity_checks)
+    ext_editor = property(get_ext_editor, set_ext_editor)
+    default_editor = property(get_default_editor, set_default_editor)
+    lock_on_ttw_edit = property(get_lock_on_ttw_edit, set_lock_on_ttw_edit)
--- plone/app/controlpanel/tests/test_bbb_editing_adapter.py
+++ plone/app/controlpanel/tests/test_bbb_editing_adapter.py
@@ -1,6 +1,8 @@
 import unittest
 from plone.app.testing import setRoles
 from zope.component import getAdapter
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 from plone.app.testing import TEST_USER_ID
 from Products.CMFCore.utils import getToolByName
 
@@ -18,71 +20,60 @@ class EditingControlPanelAdapterTest(unittest.TestCase):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        ptool = getToolByName(self.portal, 'portal_properties')
-        self.site_properties = ptool.site_properties
+        registry = getUtility(IRegistry)
+        self.settings = registry.forInterface(IEditingSchema)
 
     def test_adapter_lookup(self):
         self.assertTrue(getAdapter(self.portal, IEditingSchema))
 
     def test_get_visible_ids_setting(self):
-        self.assertEquals(self.site_properties.visible_ids, False)
-        self.site_properties.visible_ids = True
+        self.settings.visible_ids = True
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.visible_ids, True)
 
     def test_set_visible_ids_setting(self):
-        self.assertEquals(self.site_properties.visible_ids, False)
+        self.settings.visible_ids = False
         editing_settings = getAdapter(self.portal, IEditingSchema)
         editing_settings.visible_ids = True
-        self.assertEquals(self.site_properties.visible_ids, True)
+        self.assertEquals(self.settings.visible_ids, True)
 
     def test_get_enable_link_integrity_checks_setting(self):
-        self.assertEquals(
-            self.site_properties.enable_link_integrity_checks, True)
-        self.site_properties.enable_link_integrity_checks = False
+        self.settings.enable_link_integrity_checks = False
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.enable_link_integrity_checks, False)
 
     def test_set_enable_link_integrity_checks_setting(self):
-        self.assertEquals(
-            self.site_properties.enable_link_integrity_checks, True)
         editing_settings = getAdapter(self.portal, IEditingSchema)
         editing_settings.enable_link_integrity_checks = False
         self.assertEquals(
-            self.site_properties.enable_link_integrity_checks, False)
+            self.settings.enable_link_integrity_checks, False)
 
     def test_get_ext_editor_setting(self):
-        self.assertEquals(self.site_properties.ext_editor, False)
-        self.site_properties.ext_editor = True
+        self.settings.ext_editor = True
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.ext_editor, True)
 
     def test_set_ext_editor_setting(self):
-        self.assertEquals(self.site_properties.ext_editor, False)
         editing_settings = getAdapter(self.portal, IEditingSchema)
         editing_settings.ext_editor = True
-        self.assertEquals(self.site_properties.ext_editor, True)
+        self.assertEquals(self.settings.ext_editor, True)
 
     def test_get_default_editor_setting(self):
-        self.assertEquals(self.site_properties.default_editor, "TinyMCE")
-        self.site_properties.default_editor = "None"
+        self.settings.default_editor = "None"
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.default_editor, "None")
 
     def test_set_default_editor_setting(self):
-        self.assertEquals(self.site_properties.default_editor, "TinyMCE")
         editing_settings = getAdapter(self.portal, IEditingSchema)
         editing_settings.default_editor = "None"
-        self.assertEquals(self.site_properties.default_editor, "None")
+        self.assertEquals(self.settings.default_editor, "None")
 
     def test_get_lock_on_ttw_edit_setting(self):
-        self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
-        self.site_properties.lock_on_ttw_edit = False
+        self.settings.lock_on_ttw_edit = False
         editing_settings = getAdapter(self.portal, IEditingSchema)
         self.assertEquals(editing_settings.lock_on_ttw_edit, False)
 
     def test_set_lock_on_ttw_edit_setting(self):
-        self.assertEquals(self.site_properties.lock_on_ttw_edit, True)
         editing_settings = getAdapter(self.portal, IEditingSchema)
         editing_settings.lock_on_ttw_edit = False
-        self.assertEquals(self.site_properties.lock_on_ttw_edit, False)
+        self.assertEquals(self.settings.lock_on_ttw_edit, False)
