807022682d88cc707400a12725fc35455de3cdfa
Tom Gross <itconsense@gmail.com>
2014-06-26 09:51:12 +0200

    fixed some more tests

--- plone/app/controlpanel/tests/test_doctests.py
+++ plone/app/controlpanel/tests/test_doctests.py
@@ -1,4 +1,5 @@
 import doctest
+import transaction
 from unittest import TestSuite
 
 from doctest import DocFileSuite
@@ -6,8 +7,10 @@ from doctest import DocFileSuite
 from plone.testing import layered
 from plone.app.testing.bbb import PTC_FIXTURE
 
+from plone.app.controlpanel.security import migrate_to_email_login
 from plone.app.controlpanel.tests.cptc import CP_FUNCTIONAL_LAYER
 from plone.app.controlpanel.tests.cptc import simplify_white_space
+from plone.app.controlpanel.tests.cptc import generate_user_and_groups
 
 
 OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
@@ -21,7 +24,6 @@ def loginAsManager(browser, user='root', pwd='secret'):
     browser.getControl('Login Name').value = user
     browser.getControl('Password').value = pwd
     browser.getControl('Log in').click()
-
 
 
 def test_suite():
@@ -45,19 +47,23 @@ def test_suite():
         suite.addTest(layered(
             DocFileSuite(test, optionflags=OPTIONFLAGS,
                          package="plone.app.controlpanel.tests",
-                         globs={'loginAsManager': loginAsManager}),
+                         globs={'loginAsManager': loginAsManager,
+                                'simplify_white_space': simplify_white_space}),
             layer=CP_FUNCTIONAL_LAYER))
 
     suite.addTest(layered(
         DocFileSuite('usergroups.txt', optionflags=OPTIONFLAGS,
                      package="plone.app.controlpanel.tests",
-                     globs={'loginAsManager': loginAsManager}),
+                     globs={'loginAsManager': loginAsManager,
+                            'generate_user_and_groups': generate_user_and_groups}),
         layer=CP_FUNCTIONAL_LAYER))
 
     suite.addTest(layered(
         DocFileSuite('security.txt', optionflags=OPTIONFLAGS,
                      package="plone.app.controlpanel.tests",
-                     globs={'loginAsManager': loginAsManager}),
+                     globs={'loginAsManager': loginAsManager,
+                            'generate_user_and_groups': generate_user_and_groups,
+                            'migrate_to_email_login': migrate_to_email_login}),
         layer=CP_FUNCTIONAL_LAYER))
 
     return suite
