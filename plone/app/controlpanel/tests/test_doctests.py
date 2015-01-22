b0e3ae1847133827d231ebd82b1fa1ee95ebcabb
Kees Hink <keeshink@gmail.com>
2015-01-22 13:48:08 +0100

    pep8

--- plone/app/controlpanel/tests/test_doctests.py
+++ plone/app/controlpanel/tests/test_doctests.py
@@ -13,22 +13,23 @@ OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 def test_suite():
     tests = [
-#            'editing.txt',
-             'filter.txt',
-#             'mail.txt',
-#             'security_enable_user_folder.txt',
-#             'search.txt',
-#             'site.txt',
-#             'skins.txt',
-#             'markup.txt',
-#             'navigation.txt',
-             'types.txt',
-             'syndication.txt'
-             ]
+        # 'editing.txt',
+        'filter.txt',
+        # 'mail.txt',
+        # 'security_enable_user_folder.txt',
+        # 'search.txt',
+        # 'site.txt',
+        # 'skins.txt',
+        # 'markup.txt',
+        # 'navigation.txt',
+        'types.txt',
+        'syndication.txt'
+    ]
     suite = TestSuite()
 
     for test in tests:
-        suite.addTest(FunctionalDocFileSuite(test,
+        suite.addTest(FunctionalDocFileSuite(
+            test,
             optionflags=OPTIONFLAGS,
             package="plone.app.controlpanel.tests",
             test_class=ControlPanelTestCase))
