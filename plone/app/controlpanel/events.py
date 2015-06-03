f84c62edca0897648c05352d66eb68ab39e45cee
Eric BREHAULT <ebrehault@gmail.com>
2015-06-03 14:50:26 +0200

    fix module path

--- plone/app/controlpanel/events.py
+++ plone/app/controlpanel/events.py
@@ -1,5 +1,5 @@
 # Control panels for Plone 5 have been moved to CMFPlone. We keep those imports
 # for backwards compatibility.
 from Products.CMFPlone.interfaces import IConfigurationChangedEvent  # noqa
-from Products.CMFPlone.controlpanel.event import ConfigurationChangedEvent  # noqa
-from Products.CMFPlone.controlpanel.event import handleConfigurationChangedEvent  # noqa
+from Products.CMFPlone.controlpanel.events import ConfigurationChangedEvent  # noqa
+from Products.CMFPlone.controlpanel.events import handleConfigurationChangedEvent  # noqa
